e348df7fe95cc5ce0c0024d7151a97ed41d7d021
arielev <levinsonarie@gmail.com>
2018-08-04 00:54:20 +0300

    Add namespace delete support

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -25,6 +25,92 @@ function main() {
 
     export KUBECONFIG=~/.kube/config.local
 
+    action_selections=(dialog --title "Kubernetes Services Deployment" --backtitle "Kubernetes Services Deployment" --radiolist "What would like to do?" 20 50 0)
+    action_options=("Install" "" on
+             "Delete" "" off)
+    res_action_selections+=$("${action_selections[@]}" "${action_options[@]}" 2>&1 > /dev/tty)
+
+    # Return status of non-zero indicates cancel
+    if [ "$?" != "0" ]; then
+        terminate_process
+    fi
+
+    if [ ${res_action_selections} == "Install" ]; then
+        deploy_kubernetes_services
+    else
+        delete_kubernetes_services
+    fi
+
+    exit 0
+}
+
+function delete_kubernetes_services() {
+    delete_selections=(dialog --backtitle "Kubernetes Services Delete" --separate-output --checklist "Select applications to delete:" 20 50 0)
+    delete_options=("openfaas openfaas-fn" "" off
+                    "airflow" "" off)
+    res_delete_selections+=$("${delete_selections[@]}" "${delete_options[@]}" 2>&1 > /dev/tty)
+
+    # Return status of non-zero indicates cancel
+    if [ "$?" != "0" ]; then
+        terminate_process
+    fi
+
+    # selections is now: { app1 app2 app3... ... traefik_ingress }
+    # Cast selections into an Array
+    deployments_array=(${res_delete_selections// / })
+    num_of_deployments=${#deployments_array[@]}
+
+    # Check if selections made
+    if [ ${num_of_deployments} -gt 0 ]; then
+        step=$((100/num_of_deployments))  # progress bar step
+        cur_file_idx=0
+        counter=0
+        (
+        for index in ${deployments_array[@]}
+        do
+            (( counter+=step ))
+
+            echo "XXX"
+            echo "$counter"
+            echo "$counter% Deleted"
+            echo "Please wait"
+
+            echo "Deleting: $index"
+            echo "XXX"
+            processed=$((processed+1))
+
+            (( cur_file_idx+=1 )) # increase counter
+
+            [ ${counter} -gt 100 ] && break  # break when reach the 100% (or greater
+                                       # since Bash only does integer arithmetic)
+            # Main execution part
+            # Grep exact match
+            kubectl get namespaces|grep ${index}
+            # Return status of non-zero indicates cancel
+            if [ "$?" == "0" ]; then
+                kubectl delete namespace ${index}
+            fi
+        done
+        ) | dialog --backtitle "Kubernetes Services Delete" --title "Kubernetes Services Delete" --gauge "Wait please..." 20 50 0
+
+        message="Kubernetes Services Deletion Completed!\n"
+    else
+        message="You did not selected any applications to delete.\nKubernetes Services Delete Completed!\n"
+    fi
+
+    dialog --backtitle "Kubernetes Services Delete" \
+        --title "Kubernetes Services Delete" \
+        --no-cancel \
+        --msgbox "${message}" 20 50
+
+    clear
+    echo $(kubectl cluster-info)
+    echo "\n${message}"
+    return 0
+}
+
+function deploy_kubernetes_services() {
+
     dialog --backtitle "Kubernetes Services Deployment" \
         --title "Kubernetes Services Deployment" \
         --msgbox "This app will deploy various containers upon your selection.\nMake sure internet connection is available.\nPress <Enter> to continue, or <Esc> to cancel." 20 50
@@ -48,20 +134,13 @@ function main() {
                 255) terminate_process;;
             esac
         fi
-
-        deploy_kubernetes_services
     fi
 
-    exit 0
-}
-
-function deploy_kubernetes_services() {
-
-    service_selections=(dialog --backtitle "Kubernetes Services Deployment" --separate-output --checklist "Select applications to install:" 20 50 0)
-    options=("kubernetes_dashboard" "" on
+    deploy_selections=(dialog --backtitle "Kubernetes Services Deployment" --separate-output --checklist "Select applications to install:" 20 50 0)
+    deploy_options=("kubernetes_dashboard" "" on
              "openfaas" "" off
              "airflow" "" off)
-    selections+=$("${service_selections[@]}" "${options[@]}" 2>&1 > /dev/tty)
+    res_deploy_selections+=$("${deploy_selections[@]}" "${deploy_options[@]}" 2>&1 > /dev/tty)
 
     # Return status of non-zero indicates cancel
     if [ "$?" != "0" ]; then
@@ -69,11 +148,11 @@ function deploy_kubernetes_services() {
     fi
 
     # Add traefik_ingress to the array
-    selections+=' traefik_ingress'
+    res_deploy_selections+=' traefik_ingress'
 
     # selections is now: { app1 app2 app3... ... traefik_ingress }
     # Cast selections into an Array
-    deployments_array=(${selections// / })
+    deployments_array=(${res_deploy_selections// / })
     num_of_deployments=${#deployments_array[@]}
 
 
@@ -86,14 +165,13 @@ function deploy_kubernetes_services() {
     do
         (( counter+=step ))
 
-        #pct=$(( cur_file_idx * 100 / num_of_deployments ))
         echo "XXX"
         echo "$counter"
         echo "$counter% Deployed"
+        echo "Please wait"
 
         echo "Deploying: $index"
         echo "XXX"
-        #echo "$pct"
         processed=$((processed+1))
 
         (( cur_file_idx+=1 )) # increase counter
@@ -106,9 +184,10 @@ function deploy_kubernetes_services() {
     done
     ) | dialog --backtitle "Kubernetes Services Deployment" --title "Kubernetes Services Deployment" --gauge "Wait please..." 20 50 0
 
-
+    # Build urls list based on selections
     for index in ${deployments_array[@]}
     do
+        # Append to url string the return from relevant function
         urls_string="${urls_string}$(get_utl_${index}) \n"
     done
 
@@ -120,7 +199,7 @@ function deploy_kubernetes_services() {
     clear
     echo $(kubectl cluster-info)
     echo "\nKubernetes Services Deployment Completed!\nVisit: ${urls_string}"
-    exit 0
+    return 0
 }
 
 function deploy_traefik_ingress() {
@@ -129,7 +208,7 @@ function deploy_traefik_ingress() {
     # Wait until namespace created
     until kubectl get namespaces|grep ingress-traefik
     do
-        sleep 0.1
+        sleep 0.5
     done
 
     kubectl apply -f traefik/traefik-confgmap.yml
@@ -160,7 +239,7 @@ function deploy_airflow() {
     # Wait until namespace created
     until kubectl get namespaces|grep airflow
     do
-        sleep 0.1
+        sleep 0.5
     done
 
     kubectl apply -f airflow/airflow-ingress.yml
@@ -179,13 +258,13 @@ function deploy_openfaas() {
     # Wait until namespace created
     until kubectl get namespaces|grep openfaas
     do
-        sleep 0.1
+        sleep 0.5
     done
 
     # Wait until namespace created
     until kubectl get namespaces|grep openfaas-fn
     do
-        sleep 0.1
+        sleep 0.5
     done
 
     kubectl apply -f openfaas/
@@ -199,9 +278,9 @@ function get_utl_openfaas() {
 
 function execute_linux_dependencies() {
     case "$(awk -F= '/^NAME/{print $2}' /etc/os-release)" in
-        "Ubuntu"*)  sudo apt-get install -y dialog;;
+        "Ubuntu"*)          sudo apt-get install -y dialog;;
         "CentOS Linux"*)    sudo yum install -y dialog;;
-        *)          echo "Linux version not support, app terminated!" && exit 1;;
+        *)                  echo "Linux version not support, app terminated!" && exit 1;;
     esac
 }
 
