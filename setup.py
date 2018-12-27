6308d4dc444e041f5d51c27bb1435dd27e3e6421
jenkins <jenkins@nalkins.cloud>
2018-12-27 23:45:41 +0200

    update version to: 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="helm-charts",
-    version="0.0.1",
+    version="0.0.2",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Helm Charts installer",
