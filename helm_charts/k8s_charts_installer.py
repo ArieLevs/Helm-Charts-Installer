0cc0d73bb0339963f4567d31251c5b0c328f1499
arielev <levinsonarie@gmail.com>
2018-12-29 22:12:27 +0200

    Refactor urwid widgets and screens - major change

--- helm_charts/k8s_charts_installer.py
+++ helm_charts/k8s_charts_installer.py
@@ -1,98 +1,183 @@
 
 import urwid
-from init_checks import init_checks
+import urwid.raw_display
+import urwid.web_display
 from helm_functions import *
-
-
-def menu_button(caption, callback):
-    button = urwid.Button(caption)
-    urwid.connect_signal(button, 'click', callback)
-    return urwid.AttrMap(button, None, focus_map='reversed')
-
-
-def sub_menu(caption, choices):
-    contents = menu(caption, choices)
-
-    def open_menu(button):
-        return top.open_box(contents)
-    return menu_button([caption, u''], open_menu)
-
-
-def menu(title, choices):
-    body = [urwid.Text(title), urwid.Divider()]
-    body.extend(choices)
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
-
-
-def item_chosen(button):
-    response = urwid.Text([u'You chose ', button.label, u'\n'])
-    done = menu_button(u'Ok', exit_program)
-    top.open_box(urwid.Filler(urwid.Pile([response, done])))
-
-
-def terminate_chosen(button):
-    response = urwid.Text([u'Application will now terminate!'])
-    done = menu_button(u'Ok', exit_program)
-    top.open_box(urwid.Filler(urwid.Pile([response, done])))
-
-
-def exit_program(button):
-    raise urwid.ExitMainLoop()
-
-
-class CascadingBoxes(urwid.WidgetPlaceholder):
-    max_box_levels = 4
-
-    def __init__(self, box):
-        super(CascadingBoxes, self).__init__(urwid.SolidFill(u'/'))
-        self.box_level = 0
-        self.open_box(box)
-
-    def open_box(self, box):
-        self.original_widget = urwid.Overlay(urwid.LineBox(box),
-                                             self.original_widget,
-                                             align='center', width=('relative', 80),
-                                             valign='middle', height=('relative', 80),
-                                             min_width=24, min_height=8,
-                                             left=self.box_level * 3,
-                                             right=(self.max_box_levels - self.box_level - 1) * 3,
-                                             top=self.box_level * 2,
-                                             bottom=(self.max_box_levels - self.box_level - 1) * 2)
-        self.box_level += 1
-
-    def keypress(self, size, key):
-        if key == 'esc' and self.box_level > 1:
-            self.original_widget = self.original_widget[0]
-            self.box_level -= 1
-        else:
-            return super(CascadingBoxes, self).keypress(size, key)
-
-
-menu_top = menu(u'Main Menus', [
-    sub_menu(u'Install Helm Charts', [
-        sub_menu(u'Accessories', [
-            menu_button(u'Text Editor', item_chosen),
-            menu_button(u'Terminal', install_helm_charts),
-        ]),
-        menu_button(u'Terminate Application', terminate_chosen),
-    ]),
-    sub_menu(u'Remove Helm Charts', [
-        sub_menu(u'Preferences', [
-            menu_button(u'Appearance', item_chosen),
-        ]),
-        menu_button(u'Terminate Application', terminate_chosen),
-    ]),
-    menu_button(u'Terminate Application', terminate_chosen),
-])
-
-top = CascadingBoxes(menu_top)
+from init_checks import init_checks
+from kubectl_functions import *
+from helper import *
 
 
 def main():
+
+    text_header = (u"Helm charts installer -  "
+                   u"UP / DOWN / PAGE UP / PAGE DOWN scroll.  F8 exits.")
+    text_footer = remove_ansi_color_from_string(get_cluster_info().split("\n")[0])
+    text_intro = [('important', u"Helm charts installer "),
+                  u"installs kubernetes helm charts on a configured cluster, "
+                  u"by default its intended to work on a cluster on the local host. "
+                  u"\nPlease make sure to configure a .kube config file at ",
+                  ('important', u"~/.kube/config.local")]
+
+    text_repos_divider = [u"Installed ", ('important', u"Helm repositories:")]
+
+    helm_repo_installed_repositories_text = urwid.Text(identify_installed_helm_repos(return_only_decoded_string=True).replace("\t", "    "))
+    helm_repo_installed_repositories_dict = identify_installed_helm_repos()
+
+    helm_repo_name_edit_box = urwid.Edit(('editcp', u"Helm repo name "), "set name", align='left', )
+    helm_repo_url_edit_box = urwid.Edit(('editcp', u"Helm repo URL  "), "set url", align='left', )
+    helm_repo_add_result = urwid.Text('')
+
+    helm_charts_installed_dict = identify_installed_helm_charts()
+
+    text_charts_divider = [u"Installed ", ('important', u"Helm charts:")]
+    text_helm_charts_installed = identify_installed_helm_charts(return_only_decoded_string=True).replace("\t", "    ")
+
+    text_button_list = [u"Yes", u"No", u"Perhaps", u"Certainly", u"Partially", u"Tuesdays Only", u"Help"]
+
+    text_rb_list = [u"Morning", u"Afternoon", u"Evening", u"Weekend"]
+
+    def button_press(button):
+        frame.footer = urwid.AttrWrap(urwid.Text(
+            [u"Pressed: ", button.get_label()]), 'header')
+
+    def add_repo_button_pressed(button):
+        """
+        Execute once 'Add repository' button pressed,
+        function will init the 'add_helm_repo' function,
+        update the 'text_helm_repo_installed_repositories' urwid.TEXT object
+        and update 'helm_repo_add_result' urwid.TEXT object
+
+        :param button:
+        :return:
+        """
+
+        add_helm_repo_output = add_helm_repo(helm_repo_name_edit_box.edit_text,
+                                             helm_repo_url_edit_box.edit_text)['value']
+        [urwid.AttrWrap(urwid.CheckBox("{:30}{}".format(txt['repo_name'], txt['repo_url'])), 'buttn', 'buttnf')
+         for txt in identify_installed_helm_repos()]
+        helm_repo_installed_repositories_text.set_text(identify_installed_helm_repos(return_only_decoded_string=True).replace("\t", "    "))
+        helm_repo_add_result.set_text(add_helm_repo_output)
+
+    radio_button_group = []
+
+    blank = urwid.Divider()
+    listbox_content = [
+        blank,
+        urwid.Padding(urwid.Text(text_intro), left=2, right=2, min_width=20),
+
+        urwid.WidgetWrap(urwid.Divider("=", 1)),
+        urwid.Padding(urwid.Text(text_charts_divider), left=2, right=2, min_width=20),
+        urwid.WidgetWrap(urwid.Divider("*", 0, 1)),
+
+        # Display helm charts block ###########
+        # urwid.Padding(
+        #     urwid.Pile([
+        #         urwid.Text("    {:30}{}".format("NAME", "NAMESPACE")),
+        #         urwid.Pile(
+        #             [urwid.AttrWrap(urwid.CheckBox("{:30}{}".format(chart['chart_name'], chart['name_space'])), 'buttn',
+        #                             'buttnf') for chart in helm_charts_installed_dict],
+        #         ),
+        #
+        #     ]), left=2, right=2, min_width=10),
+        #######################################
+        # Helm charts display
+        urwid.Padding(urwid.Text(text_helm_charts_installed), left=2, right=2, min_width=20),
+
+        urwid.WidgetWrap(urwid.Divider("=", 1)),
+        urwid.Padding(urwid.Text(text_repos_divider), left=2, right=2, min_width=20),
+        urwid.WidgetWrap(urwid.Divider("*", 0, 1)),
+
+        # Display helm repositories block #####
+        # urwid.Padding(
+        #     urwid.Pile([
+        #         urwid.Text("    {:30}{}".format("NAME", "URL")),
+        #         urwid.Pile(
+        #             [urwid.AttrWrap(urwid.CheckBox("{:30}{}".format(repo['repo_name'], repo['repo_url'])), 'buttn',
+        #                             'buttnf') for repo in helm_repo_installed_repositories_dict],
+        #         ),
+        #
+        #     ]), left=2, right=2, min_width=10),
+        #######################################
+
+        # Helm repositories display
+        urwid.Padding(helm_repo_installed_repositories_text, left=2, right=2, min_width=20),
+        blank,
+        urwid.Padding(urwid.AttrWrap(helm_repo_name_edit_box, 'editbx', 'editfc'), left=2, right=2),
+        urwid.Padding(urwid.AttrWrap(helm_repo_url_edit_box, 'editbx', 'editfc'), left=2, right=2),
+        blank,
+        urwid.Padding(urwid.AttrWrap(urwid.Button("Add repository", add_repo_button_pressed), 'buttn', 'buttnf'), left=2, right=2, width=18),
+        blank,
+        urwid.Padding(helm_repo_add_result, left=2, right=2, min_width=20),
+        blank,
+
+        # urwid.WidgetWrap(urwid.Columns([
+        #     urwid.Divider("#"),
+        #     urwid.Divider('$'),
+        #     urwid.Divider("&"),
+        #     urwid.Divider('"'),
+        #     urwid.Divider("'"),
+        #     ])),
+        #
+        # blank,
+        # urwid.Padding(urwid.GridFlow(
+        #     [urwid.AttrWrap(urwid.Button(txt, button_press), 'buttn','buttnf') for txt in text_button_list],
+        #     13, 3, 1, 'left'),
+        #     left=4, right=3, min_width=13),
+        #
+        # blank,
+        # urwid.Padding(urwid.GridFlow(
+        #     [urwid.AttrWrap(urwid.RadioButton(radio_button_group, txt), 'buttn','buttnf')
+        #         for txt in text_rb_list],
+        #     13, 3, 1, 'left') ,
+        #     left=4, right=3, min_width=13),
+        # blank,
+        ]
+
+    header = urwid.AttrWrap(urwid.Text(text_header), 'header')
+    footer = urwid.AttrWrap(urwid.Text(text_footer), 'footer')
+    listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
+
+    # Frame widget is used to keep the instructions at the top of the screen
+    frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header, footer=footer)
+
+    palette = [
+        ('body', 'black', 'light gray', 'standout'),
+        ('reverse', 'light gray', 'black'),
+        ('header', 'white', 'dark red', 'bold'),
+        ('footer', 'white', 'dark green', 'bold'),
+        ('important', 'dark blue', 'light gray', ('standout', 'underline')),
+        ('editfc', 'white', 'dark blue', 'bold'),
+        ('editbx', 'light gray', 'dark blue'),
+        ('editcp', 'black', 'light gray', 'standout'),
+        ('bright', 'dark gray', 'light gray', ('bold', 'standout')),
+        ('buttn', 'black', 'light blue'),
+        ('buttnf', 'white', 'dark blue', 'bold'),
+        ]
+
+    # use appropriate Screen class
+    if urwid.web_display.is_web_request():
+        screen = urwid.web_display.Screen()
+    else:
+        screen = urwid.raw_display.Screen()
+
+    def unhandled(key):
+        if key == 'f8':
+            raise urwid.ExitMainLoop()
+
+    urwid.MainLoop(frame, palette, screen, unhandled_input=unhandled).run()
+
+
+def setup():
+    urwid.web_display.set_preferences("Urwid Tour")
+    # try to handle short web requests quickly
+    if urwid.web_display.handle_short_request():
+        return
+
+    main()
+
+
+if '__main__' == __name__ or urwid.web_display.is_web_request():
     print("Starting applications, please wait...")
     init_checks()
-    urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
-
-
-if __name__ == "__main__":
-    main()
+    setup()
