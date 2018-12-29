e5c7a5b1732ca66245bcf8acf7858645b12fe99b
arielev <levinsonarie@gmail.com>
2018-12-29 22:23:08 +0200

    Release first major version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="helm-charts",
-    version="0.0.4",
+    version="1.0.0",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Helm Charts installer",
